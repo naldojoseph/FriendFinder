1f38db3b82e6870067ee522d666457a6a1e16ecb
Naldo Tumundo <naldo_j3@yahoo.com>
2019-10-23 13:52:42 -0700

    friend.js added

---
+++ app/data/friend.js
@@ -0,0 +1,43 @@
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "Blake",
+      photo: "https://media1.popsugar-assets.com/files/thumbor/5klCp1M6Ump79GgVnka-4fcc2vo/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2012/12/51/4/192/1922398/00531a6d9210fa39_blakelively/i/Blake-Lively.jpg",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Alexandra",
+      photo:"https://i.pinimg.com/originals/72/2f/2d/722f2d2ed3da89266b487fef2b2ca828.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Miss Sunday",
+      photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Nathalie_Emmanuel_by_Gage_Skidmore.jpg/640px-Nathalie_Emmanuel_by_Gage_Skidmore.jpg?1571862657918",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Jessica",
+      photo:"https://img1.cookinglight.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2014/12/main/1501p54-jessica-alba.jpg?itok=uok8Hr-T",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Margot",
+      photo:"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-img.instyle.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F684xflex%2Fpublic%2F1474907261%2F092616-margot-robbie-lead.jpg%3Fitok%3DforuErJe&q=85",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Kat",
+      photo:"https://www.thewrap.com/wp-content/uploads/2016/05/KatGraham.jpg",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Emma",
+      photo:"https://timedotcom.files.wordpress.com/2015/12/gettyimages-485360238.jpg",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
